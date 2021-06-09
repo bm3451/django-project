c62f933bfb06d206088e41827e0f4c0b262d13a1
minjun <minjun.co.kr>
2021-06-09 17:36:10 +0900

    comp 2-07

--- pybo/urls.py
+++ pybo/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+
 ]
