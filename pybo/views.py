101a334b39e3674d18adfbac58008b5d9178e0c4
seoyoung <bm3451@naver.com>
2021-06-10 11:37:06 +0900

    부트스트랩 적용 및 확인 완료

--- pybo/views.py
+++ pybo/views.py
@@ -8,6 +8,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from django.utils import timezone
 
+from .forms import QuestionForm, AnswerForm
+
 def index(request):
     """
     pybo 목록 출력
@@ -22,8 +24,17 @@ def detail(request, question_id):
     pybo 내용 출력
     """
     question = get_object_or_404(Question, pk=question_id)
-    question = Question.objects.get(id=question_id)
-    context = {'question': question}
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
 def answer_create(request, question_id):
@@ -33,3 +44,19 @@ def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
+
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
